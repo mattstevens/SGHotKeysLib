0eb42ad2018a04a9a29fdcc097413c7ee27c82d1
Roustem Karimov <roustem@agilebits.com>
2011-07-22 01:16:30 +0800

    Simplifying, removing unused code.

--- SGHotKeysLib/SGHotKeyCenter.m
+++ SGHotKeysLib/SGHotKeyCenter.m
@@ -14,16 +14,16 @@
 OSType const kHotKeySignature = 'SGHk';
 
 @interface SGHotKeyCenter ()
+
 - (SGHotKey *)_hotKeyForCarbonHotKey:(EventHotKeyRef)carbonHotKey;
 - (EventHotKeyRef)_carbonHotKeyForHotKey:(SGHotKey *)hotKey;
 
 - (void)_updateEventHandler;
 - (void)_hotKeyDown: (SGHotKey *)hotKey;
-- (void)_hotKeyUp: (SGHotKey *)hotKey;
-static OSStatus hotKeyEventHandler(EventHandlerCallRef inHandlerRef, EventRef inEvent, void* refCon );
+
 @end
 
-
+static OSStatus hotKeyEventHandler(EventHandlerCallRef inHandlerRef, EventRef inEvent, void* refCon );
 static SGHotKeyCenter *sharedCenter = nil;
 
 @implementation SGHotKeyCenter
@@ -35,88 +35,89 @@ static SGHotKeyCenter *sharedCenter = nil;
 }
 
 - (void)dealloc {
-  [hotKeys release];
-  [super dealloc];
+	[hotKeys release];
+	[super dealloc];
 }
 
 + (SGHotKeyCenter *)sharedCenter {    
-  return sharedCenter;
+	return sharedCenter;
 }
 
 + (id) allocWithZone:(NSZone *)zone {
-  //Usually already set by +initialize.
-  if (sharedCenter) {
-    //The caller expects to receive a new object, so implicitly retain it to balance out the caller's eventual release message.
-    return [sharedCenter retain];
-  } else {
-    //When not already set, +initialize is our caller—it's creating the shared instance. Let this go through.
-    return [super allocWithZone:zone];
-  }
+	//Usually already set by +initialize.
+	if (sharedCenter) {
+		//The caller expects to receive a new object, so implicitly retain it to balance out the caller's eventual release message.
+		return [sharedCenter retain];
+	} else {
+		//When not already set, +initialize is our caller—it's creating the shared instance. Let this go through.
+		return [super allocWithZone:zone];
+	}
 }
 
 - (id) init {
-  if (!hasInited) {
-    if ((self = [super init])) {
-      //Initialize the instance here.
+	if (!hasInited) {
+		if ((self = [super init])) {
+			//Initialize the instance here.
 			hotKeys = [[NSMutableDictionary alloc] init];    
-      hasInited = YES;
-    }
-  }
+			hasInited = YES;
+		}
+	}
 	
-  return self;
+	return self;
 }
 
 - (BOOL)registerHotKey:(SGHotKey *)theHotKey {
-  OSStatus error;
-  EventHotKeyID hotKeyID;
-  EventHotKeyRef carbonHotKey;
-  NSValue *key = nil;
-  
-  if ([[self allHotKeys] containsObject:theHotKey])
-    [self unregisterHotKey:theHotKey];
-  
-  if (![[theHotKey keyCombo] isValidHotKeyCombo])
-    return YES;
-  
-  static UInt32 currentId = 0;
-  hotKeyID.signature = kHotKeySignature;
-  hotKeyID.id = ++currentId;
-  
-  theHotKey.hotKeyID = hotKeyID;
-  
-  error = RegisterEventHotKey(theHotKey.keyCombo.keyCode,
-                              theHotKey.keyCombo.modifiers,
-                              hotKeyID,
-                              GetEventDispatcherTarget(),
-                              0,
-                              &carbonHotKey);
-  
-  if (error) 
-    return NO;
-  
-  key = [NSValue valueWithPointer:carbonHotKey];
-  
-  if (theHotKey && key)
-    [hotKeys setObject:theHotKey forKey:key];
-  
-  [self _updateEventHandler];
-  
-  return YES;
+	OSStatus error;
+	EventHotKeyID hotKeyID;
+	EventHotKeyRef carbonHotKey;
+	NSValue *key = nil;
+	
+	if ([[self allHotKeys] containsObject:theHotKey])
+		[self unregisterHotKey:theHotKey];
+	
+	if (![[theHotKey keyCombo] isValidHotKeyCombo])
+		return YES;
+	
+	static UInt32 currentId = 0;
+	hotKeyID.signature = kHotKeySignature;
+	hotKeyID.id = ++currentId;
+	
+	theHotKey.hotKeyID = hotKeyID;
+	
+	error = RegisterEventHotKey(theHotKey.keyCombo.keyCode,
+								theHotKey.keyCombo.modifiers,
+								hotKeyID,
+								GetEventMonitorTarget(),
+								// GetEventDispatcherTarget(),
+								0,
+								&carbonHotKey);
+	
+	if (error) 
+		return NO;
+	
+	key = [NSValue valueWithPointer:carbonHotKey];
+	
+	if (theHotKey && key)
+		[hotKeys setObject:theHotKey forKey:key];
+	
+	[self _updateEventHandler];
+	
+	return YES;
 }
 
 
 - (void)unregisterHotKey:(SGHotKey *)theHotKey {
-  EventHotKeyRef carbonHotKey;
-  NSValue *key = nil;
-  
-  if (![[self allHotKeys] containsObject:theHotKey])
+	EventHotKeyRef carbonHotKey;
+	NSValue *key = nil;
+	
+	if (![[self allHotKeys] containsObject:theHotKey])
 		return;
 	
 	carbonHotKey = [self _carbonHotKeyForHotKey:theHotKey];
 	NSAssert(carbonHotKey != nil, @"");
-  
+	
 	UnregisterEventHotKey(carbonHotKey);
-  
+	
 	key = [NSValue valueWithPointer:carbonHotKey];
 	[hotKeys removeObjectForKey:key];
 	
@@ -124,107 +125,99 @@ static SGHotKeyCenter *sharedCenter = nil;
 }
 
 - (NSArray *)allHotKeys {
-  return [hotKeys allValues];
+	return [hotKeys allValues];
 }
 
 
 - (SGHotKey *)hotKeyWithIdentifier:(id)theIdentifier {
-  if (!theIdentifier)
-    return nil;
-  
-  for (SGHotKey *hotKey in [self allHotKeys]) {
-    if([[hotKey identifier] isEqual:theIdentifier] )
+	if (!theIdentifier)
+		return nil;
+	
+	for (SGHotKey *hotKey in [self allHotKeys]) {
+		if([[hotKey identifier] isEqual:theIdentifier] )
 			return hotKey;
-  }
-  
-  return nil;
+	}
+	
+	return nil;
 }
 
-- (OSStatus)sendCarbonEvent:(EventRef)event {
-  OSStatus error;
-  EventHotKeyID hotKeyID;
-  SGHotKey *hotKey = nil;
-
-  NSAssert(GetEventClass(event) == kEventClassKeyboard, @"Unknown event class");
-
-  error = GetEventParameter(event,
-                            kEventParamDirectObject, 
-                            typeEventHotKeyID,
-                            nil,
-                            sizeof(EventHotKeyID),
-                            nil,
-                            &hotKeyID);
-  if (error)
-    return error;
-
-  NSAssert(hotKeyID.signature == kHotKeySignature, @"Invalid hot key id");
-  NSAssert(hotKeyID.id != 0, @"Invalid hot key id");
-
-  for (SGHotKey *thisHotKey in [self allHotKeys]) {
-    if ([thisHotKey matchesHotKeyID:hotKeyID]) {
-      hotKey = thisHotKey;
-      break;
-    }
-  }
- 
-  switch (GetEventKind(event)) {
-    case kEventHotKeyPressed:
-      [self _hotKeyDown:hotKey];
-      break;
-
-    case kEventHotKeyReleased:
-      [self _hotKeyUp:hotKey];
-      break;
-      
-    default:
-      NSAssert(0, @"Unknown event kind");
-      break;
-  }
-
-  return noErr;
+- (OSStatus)processCarbonEvent:(EventRef)event {
+	OSStatus error;
+	EventHotKeyID hotKeyID;
+	SGHotKey *hotKey = nil;
+	
+	NSAssert(GetEventClass(event) == kEventClassKeyboard, @"Unknown event class");
+	
+	error = GetEventParameter(event,
+							  kEventParamDirectObject, 
+							  typeEventHotKeyID,
+							  nil,
+							  sizeof(EventHotKeyID),
+							  nil,
+							  &hotKeyID);
+	if (error)
+		return error;
+	
+	NSAssert(hotKeyID.signature == kHotKeySignature, @"Invalid hot key id");
+	NSAssert(hotKeyID.id != 0, @"Invalid hot key id");
+	
+	for (SGHotKey *thisHotKey in [self allHotKeys]) {
+		if ([thisHotKey matchesHotKeyID:hotKeyID]) {
+			hotKey = thisHotKey;
+			break;
+		}
+	}
+	
+	switch (GetEventKind(event)) {
+		case kEventHotKeyPressed:
+			[self _hotKeyDown:hotKey];
+			break;
+			
+		default:
+			NSAssert(0, @"Unknown event kind");
+			break;
+	}
+	
+	return eventNotHandledErr;
 }
 
 - (SGHotKey *)_hotKeyForCarbonHotKey:(EventHotKeyRef)carbonHotKey {
-  NSValue *key = [NSValue valueWithPointer:carbonHotKey];
+	NSValue *key = [NSValue valueWithPointer:carbonHotKey];
 	return [hotKeys objectForKey:key];
 }
 
 - (EventHotKeyRef)_carbonHotKeyForHotKey:(SGHotKey *)hotKey {
-  NSArray *values;
-  NSValue *value;
-
-  values = [hotKeys allKeysForObject:hotKey];
-  NSAssert([values count] == 1, @"Failed to find Carbon Hotkey for SGHotKey");
-
-  value = [values lastObject];
-
-  return (EventHotKeyRef)[value pointerValue];
+	NSArray *values;
+	NSValue *value;
+	
+	values = [hotKeys allKeysForObject:hotKey];
+	NSAssert([values count] == 1, @"Failed to find Carbon Hotkey for SGHotKey");
+	
+	value = [values lastObject];
+	
+	return (EventHotKeyRef)[value pointerValue];
 }
 
 - (void)_updateEventHandler {  
+	static EventHandlerUPP handler = NewEventHandlerUPP(hotKeyEventHandler);
+	
 	if ([hotKeys count] && eventHandlerInstalled == NO) {
-		EventTypeSpec eventSpec[2] = {
-			{ kEventClassKeyboard, kEventHotKeyPressed },
-			{ kEventClassKeyboard, kEventHotKeyReleased }
+		EventTypeSpec eventSpec[1] = {
+			{ kEventClassKeyboard, kEventHotKeyPressed }
 		};
-    
-		InstallEventHandler(GetEventDispatcherTarget(), 
-                        (EventHandlerProcPtr)hotKeyEventHandler, 
-                        2, eventSpec, nil, nil);
-    
+		
+		InstallEventHandler(GetEventMonitorTarget(), handler, 1, eventSpec, NULL, NULL);
 		eventHandlerInstalled = YES;
 	}  
 }
 
 - (void)_hotKeyDown:(SGHotKey *)hotKey {
-  [hotKey invoke];
-}
-
-- (void)_hotKeyUp:(SGHotKey *)hotKey {
-  // Nothing!
+	[hotKey invoke];
 }
 
 static OSStatus hotKeyEventHandler(EventHandlerCallRef theHandlerRef, EventRef theEvent, void *userData ) {
-  return [[SGHotKeyCenter sharedCenter] sendCarbonEvent:theEvent];
+	OSStatus result = [[SGHotKeyCenter sharedCenter] processCarbonEvent:theEvent];
+	return result;
 }
+
 @end
